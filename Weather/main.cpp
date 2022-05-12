e6add6c2bcfe30be52a0022db56cdcd6dc005a52
Hyo Chang Kim <26371358+hckim16@users.noreply.github.com>
2022-05-12 09:45:12 -0400

    Add files via upload

---
+++ Weather/main.cpp
@@ -0,0 +1,47 @@
+//
+//  main.cpp
+//  Weather Statistics
+//
+//  Created by Hyo Kim on 5/12/22.
+//
+
+#include <iostream>
+#include <string>
+
+#include "weatherStats.hpp"
+
+using namespace std;
+
+int main() {
+
+    WeatherStatistics stats;
+    
+    // request start date/time from user
+    string startDate, startTime;
+    cout << endl << "Enter START DATE as yyyy_mm_dd: ";
+    cin >> startDate;
+    cout << endl << "Enter START TIME as hh:mm:ss (24-hour): ";
+    cin >> startTime;
+    
+    if (!stats.isValidDateTime(startDate, startTime)) return 1;
+    
+    // request end date/time from user
+    string endDate, endTime;
+    cout << endl << "Enter END DATE as yyyy_mm_dd: ";
+    cin >> endDate;
+    cout << endl << "Enter START TIME as hh:mm:ss (24-hour): ";
+    cin >> endTime;
+    
+    if (!stats.isValidDateTime(endDate, endTime)) return 1;
+    
+    double coeff = stats.computeCoeff(startDate, startTime, endDate, endTime);
+    
+    cout << endl << "Coefficient: " << coeff * 24 * 60 * 6-0 << " inHg/day" << endl;
+    
+    // determin if it's sunny or stormy
+    
+    if (coeff < 0) cout << "Stormy conditions... Looks like you might need an umbrella!" << endl;
+    else cout << "Sunny conditions... Don't forget your sunscreen!" << endl;
+    
+    return 0;
+}
---
+++ Weather/weatherStats.hpp
@@ -0,0 +1,35 @@
+//
+//  weatherStats.hpp
+//  Weather Statistics
+//
+//  Created by Hyo Kim on 5/12/22.
+//
+
+#ifndef weatherStats_hpp
+#define weatherStats_hpp
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <map>
+#include <ctime>
+
+#define RESOURCE_PATH "../Resources/"
+
+class WeatherStatistics {
+public:
+    WeatherStatistics();
+    bool isValidDateTime(std::string date, std::string time);
+    double computeCoeff(std::string startDate, std::string startTime, std::string endDate, std::string endTime);
+    
+private:
+    std::map<time_t, double> timeToPressure_;
+    
+    void loadData();
+    time_t getFirstDateTime();
+    time_t getLastDateTime();
+    time_t convertDateTime(std::string date, std::string time);
+};
+
+#endif /* weatherStats_hpp */
